13065cb76b0f9a04e98934d3c878f69712ee1fd5
Kanaiya Mayank <academic.mayankkanaiya03@gmail.com>
2025-12-24 10:13:25 +0530

    init Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-neutral-950 text-2xl text-white flex justify-center items-center'>
+      <Login />
+
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className=' p-5 flex justify-center items-center flex-col gap-3'>
+        <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Login
